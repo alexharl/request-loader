3d04c288e1ccb753820b2469a2945e3f19c8cb02
Alexander Harl <a.harl@symanics.com>
2018-07-27 02:26:51 +0200

    added JS-Doc Comments

--- src/simple-event-scraper.js
+++ src/simple-event-scraper.js
@@ -2,29 +2,39 @@ const rp = require("request-promise");
 const cheerio = require("cheerio");
 
 module.exports = class SimpleEventScraper {
+    /**
+     * 
+     * @param {*} uri url where events are listed 
+     * @param {*} eventCtor Constructor to use to instantiate new events
+     */
     constructor(uri, eventCtor) {
         this.uri = uri;
         this.eventCtor = eventCtor;
         this.events = [];
     }
-    transform(body) {
-        return cheerio.load(body);
-    }
-    async load() {
-        return await rp({ uri: this.uri, transform: this.transform });
-    }
+
+    /**
+     * @returns { this.eventCtor[] } list of events
+     */
     async loadEvents() {
         const $ = await this.load();
         if (!$) throw new Error('could not load events.');
 
         let events = [];
-        /* iteriere über alle event container, wie diese identifiziert werden, weis die Event Klasse */
+        /* iterate event containers */
         $(this.eventCtor.LIST_CONTAINER).each((i, elem) => {
-            /* für jeden container 'elem' wird die statische methode parse vom Event aufgerufen */
+            /* for each 'elem' Event's static method 'parse' is used to instantiate an event */
             const event = this.eventCtor.parse($, elem);
-            if (event) events.push(event); // hat das erstellen geklappt -> ab in events
+            if (event) events.push(event); // add event to events array
         });
-        
+
         return (this.events = events);
+    }
+    
+    async load() {
+        return await rp({ uri: this.uri, transform: this.transform });
+    }
+    transform(body) {
+        return cheerio.load(body);
     }
 }
